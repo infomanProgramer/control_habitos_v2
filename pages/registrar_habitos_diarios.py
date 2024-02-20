9d7a98059a7d4081bdcb0f6802a2a5e2770821fc
Rafael Ortiz <rfortiz@bg.com.bo>
2024-02-19 22:54:10 -0400

    reseteo de la bd

--- pages/registrar_habitos_diarios.py
+++ pages/registrar_habitos_diarios.py
@@ -2,6 +2,8 @@ import streamlit as st
 import sqlite3
 from pages.nuevo_habito import fetchCategoriaHabitos
 from pages.lista_habitos import getListaHabitosAll
+from pages.Utils.libraryBD import addNewItemDB
+from pages.Utils.libraryBD import createObjectDB
 
 
 def registrar_habitos_diarios():
@@ -16,6 +18,23 @@ def registrar_habitos_diarios():
 
     with st.form("my_form"):
         options = st.multiselect("Registro habitos", valores_habitos)
-        saveBtn = st.form_submit_button('Submit my picks')
-        
+        saveBtn = st.form_submit_button('Guardar')
+        optionsIndex = []
+        if saveBtn:
+            print(options)
+            for option in options:
+                for habito in lista_habitos:
+                    if habito["value"] == option:
+                        optionsIndex.append(habito["key"])
+            print(optionsIndex)
+            for optionsI in optionsIndex:
+                registroHabitos = []
+                registroHabitos.append(createObjectDB("ID_HABITO", optionsI, "I"))
+                registroHabitos.append(createObjectDB("ESTADO", True, "B"))
+                registroHabitos.append(createObjectDB("FECHA_REGISTRO", "datetime('now', 'localtime')", "I"))
+                cursor.execute(addNewItemDB("SEGUIMIENTOHABITOS", registroHabitos))
+            conn.commit()
+            conn.close()
+            
+
         
